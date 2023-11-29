4a5870fd3179272e1f58cec841e82c7702b04a68
David Olivera <davidolivera8989@gmail.com>
2023-11-29 02:04:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, make_response, request, render_template, session
+from flask_sqlalchemy import SQLAlchemy
+import jwt
+from datetime import datetime, timedelta
+from functools import wraps
+from psycopg2 import connect
+from flask_cors import CORS, cross_origin
+import json
+import numpy as np
+from scipy.linalg import inv
 
 app = Flask(__name__)
 
