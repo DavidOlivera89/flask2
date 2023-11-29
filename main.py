ecb5bddcdcc0c77ba51d1745c4a5cb4825e555e7
David Olivera <davidolivera8989@gmail.com>
2023-11-29 01:59:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return Choo Choo": "Welcome to your Flask app 🚅
+    return 'Choo Choo": "Welcome to your Flask app'
 
 if __name__ == '__main__':
   app.run(port=5000)
