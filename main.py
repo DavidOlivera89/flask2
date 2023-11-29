d6ff86fd3ac9447b78663d579f045b800504ada5
David Olivera <davidolivera8989@gmail.com>
2023-11-29 02:01:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-from flask import Flask, request, jsonify, make_response, request, render_template, session
-from flask_sqlalchemy import SQLAlchemy
-import jwt
-from datetime import datetime, timedelta
-from functools import wraps
-from psycopg2 import connect
-from flask_cors import CORS, cross_origin
-import json
-import numpy as np
-from scipy.linalg import inv
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return 'Choo Choo": "Welcome to your Flask app'
+  return render_template('index.html')
 
 if __name__ == '__main__':
   app.run(port=5000)
-
-
 
